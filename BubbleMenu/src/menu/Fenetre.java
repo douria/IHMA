d2e9e228b5bf0380c1f4457e86d89c249d59b98c
alice2909 <alice.rivoal@gmail.com>
2017-11-24 10:41:48 +0100

    projet

---
+++ BubbleMenu/src/menu/Fenetre.java
@@ -0,0 +1,9 @@
+package menu;
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+
+public class Fenetre {
+
+}
---
+++ BubbleMenu/src/menu/Main.java
@@ -0,0 +1,13 @@
+package menu;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
