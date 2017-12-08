3dee2c7441f9585a76a61093ae42b02655fe94c1
alice2909 <alice.rivoal@gmail.com>
2017-12-08 09:55:17 +0100

    separe menu fenetre

--- BubbleMenu/src/menu/Fenetre.java
+++ BubbleMenu/src/menu/Fenetre.java
@@ -1,9 +1,31 @@
 package menu;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JMenu;
+import javax.swing.JRadioButtonMenuItem;
 
-public class Fenetre {
+public class Fenetre extends JFrame{
+	private static final long serialVersionUID = 1L;	
+	private MenuClassique menuBar = new MenuClassique();
 
+	public static void main(String[] args){
+		Fenetre zFen = new Fenetre();
+	}
+	
+	public Fenetre(){
+		this.setSize(400, 600);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		
+		this.setJMenuBar(menuBar);
+		
+		this.setVisible(true);
+	}
+		
 }
